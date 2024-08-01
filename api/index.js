5b7b64906d02a3110d8fb1cb4dbea01c7fddd4bc
Chinhong <75408733+rexes123@users.noreply.github.com>
2024-08-01 11:48:45 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-// const { Pool } = require("pg");
+const { Pool } = require("pg");
 // const cors = require("cors");
 // require('dotenv').config();
 
