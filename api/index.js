edfcb876402b57596bec56c149febf0fc0e163f3
Chinhong <75408733+rexes123@users.noreply.github.com>
2024-08-01 11:42:13 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,12 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-const { Pool } = require("pg");
-const cors = require("cors");
-require('dotenv').config();
+// const { Pool } = require("pg");
+// const cors = require("cors");
+// require('dotenv').config();
 
 // const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
 
 
@@ -23,66 +23,66 @@ app.get("/test", (req, res) => {
   res.json({ message: "Test route working" });
 });
 
-const { DATABASE_URL, SECRET_KEY } = process.env;
+// const { DATABASE_URL, SECRET_KEY } = process.env;
 
 // Initialize the PostgreSQL connection
-const pool = new Pool({
-  connectionString: DATABASE_URL,
+// const pool = new Pool({
+//   connectionString: DATABASE_URL,
 
-  ssl: {
-    // require: true,
-    rejectUnauthorized: false,
-  },
-});
+//   ssl: {
+//     // require: true,
+//     rejectUnauthorized: false,
+//   },
+// });
 
-(async () => {
-  try {
-    const client = await pool.connect();
-    console.log('Database connected successfully');
-    client.release();
-  } catch (err) {
-    console.error('Database connection error:', err);
-  }
-})();
+// (async () => {
+//   try {
+//     const client = await pool.connect();
+//     console.log('Database connected successfully');
+//     client.release();
+//   } catch (err) {
+//     console.error('Database connection error:', err);
+//   }
+// })();
 
-// Function to fetch PostgreSQL version
-async function getPostgreSQLVersion() {
-  const client = await pool.connect();
-  try {
-    const response = await client.query("SELECT VERSION()");
-    // Print the first row containing version info
-    console.log(response.rows[0]);
-    // Return version information
-    return response.rows[0];
-  } catch (error) {
-    console.error('Error:', error);
-    res.status(500).send('Server Error');
-  } finally {
-    client.release();
-  }
-}
-getPostgreSQLVersion();
+// // Function to fetch PostgreSQL version
+// async function getPostgreSQLVersion() {
+//   const client = await pool.connect();
+//   try {
+//     const response = await client.query("SELECT VERSION()");
+//     // Print the first row containing version info
+//     console.log(response.rows[0]);
+//     // Return version information
+//     return response.rows[0];
+//   } catch (error) {
+//     console.error('Error:', error);
+//     res.status(500).send('Server Error');
+//   } finally {
+//     client.release();
+//   }
+// }
+// getPostgreSQLVersion();
 
-//fetch all post
-app.get("/posts", async (req, res) => {
-  const client = await pool.connect();
-  try {
-    //SQL query
-    const query = "SELECT * FROM ALLPOSTS ORDER BY create_at ASC";
-    console.log(query);
+// //fetch all post
+// app.get("/posts", async (req, res) => {
+//   const client = await pool.connect();
+//   try {
+//     //SQL query
+//     const query = "SELECT * FROM ALLPOSTS ORDER BY create_at ASC";
+//     console.log(query);
 
-    //Execute sql quete
-    const execute = await client.query(query);
-    console.log(execute);
-    //Response to client
-    res.status(200).json(execute.rows);
-  } catch (error) {
-    console.error(error.message);
-    res.status(500).send('Server Error');
-  } finally {
-    client.release();
-  }
-});
+//     //Execute sql quete
+//     const execute = await client.query(query);
+//     console.log(execute);
+//     //Response to client
+//     res.status(200).json(execute.rows);
+//   } catch (error) {
+//     console.error(error.message);
+//     res.status(500).send('Server Error');
+//   } finally {
+//     client.release();
+//   }
+// });
 
 // app.get("/posts/:id", async (req, res) => {
 //   const client = await pool.connect();
